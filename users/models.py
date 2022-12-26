6a2404842f751b62f937216c4e03e6415f2cd798
Sahb <msahbet@yandex.ru>
2022-12-26 20:18:28 +0300

    10th lecture

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+	def __str__(self):
+		return f'{self.user.username} Profile'
+
+	def save(self):
+		super().save()
+		img = Image.open(self.image.path)
+		s = 300
+		if img.height > s or img.width > s:
+			output_size = (s, s)
+			img.thumbnail(output_size)
+			img.save(self.image.path)
